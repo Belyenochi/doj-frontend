93903d25743bac33fa75bc5197cebcb9b90a9097
ChouUn <ChouUnSoft@gmail.com>
2016-11-01 03:58:54 +0800

    increase package jquery, and add style of bootstrap to index.pug

--- conf/webpack.init.config.js
+++ conf/webpack.init.config.js
@@ -8,7 +8,7 @@ module.exports = {
   cache: true,
   devtool: 'cheap-source-map',
   entry: {
-    'lib_01_bootstrap': ['bootstrap'],
+    'lib_01_bootstrap': ['jquery', 'bootstrap'],
     'lib_02_react': ['react', 'react-dom', 'react-router'],
     'lib_03_material': ['material-ui', 'react-tap-event-plugin'],
     'lib_04_others': ['react-title-component', 'keycode', 'history'],
